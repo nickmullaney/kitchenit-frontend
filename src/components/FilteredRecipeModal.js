81ac38d5821e895bfb53763d29348fea9d8bad7c
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2023-03-06 14:24:16 -0800

    Adds style

--- src/components/FilteredRecipeModal.js
+++ src/components/FilteredRecipeModal.js
@@ -6,11 +6,11 @@ import './FilteredRecipeModal.css';
 class FilteredRecipeModal extends React.Component {
   render() {
     const recipe = this.props.recipe;
-    return(
+    return (
       <Modal
         size='lg'
         show={this.props.show}
-           onHide={this.props.close}
+        onHide={this.props.close}
         centered
         className='filteredBackground'
       >
@@ -26,7 +26,7 @@ class FilteredRecipeModal extends React.Component {
             alt={recipe.name}
             title={recipe.name}
           />
-          
+
           <div className="recipe-modal-ingredients">
             <h2>Ingredients</h2>
             {this.props.comparedIngredients.map((ingredient, i) => (
@@ -40,26 +40,26 @@ class FilteredRecipeModal extends React.Component {
                   }}
                   src={ingredient.imageUrl}
                   alt={ingredient.ingredientName}
-                  title={ingredient.ingredientName}/>
+                  title={ingredient.ingredientName} />
                 <h4>{ingredient.ingredientName}</h4>
                 <h4>{ingredient.measurement}</h4>
               </div>
-              
+
             ))}
           </div>
           <div className="instructions">
             <h2>Instructions</h2>
             <ol>
               {recipe.instructions.map((step, i) => (
-                <li key={i+1}>{step}</li>
+                <li key={i + 1}>{step}</li>
               ))}
             </ol>
           </div>
         </Modal.Body>
-        <Modal.Footer className='filteredFooter'> 
+        <Modal.Footer className='filteredFooter'>
           <button className='closeFilteredModal' onClick={this.props.close}>Close</button>
-          {this.props.hasInCookbook ? <button className='filteredRecipeRemove' onClick={(e) => this.props.handleDeleteRecipe(e, this.props.id)}>Remove from Cookbook</button> : <button className='filteredAdd' onClick={(e) => this.props.handleAddRecipe(e, recipe)}>Add to Cookbook</button>         
-            }
+          {this.props.hasInCookbook ? <button className='filteredRecipeRemove' onClick={(e) => this.props.handleDeleteRecipe(e, this.props.id)}>Remove from Cookbook</button> : <button className='filteredAdd' onClick={(e) => this.props.handleAddRecipe(e, recipe)}>Add to Cookbook</button>
+          }
         </Modal.Footer>
       </Modal>
     );
