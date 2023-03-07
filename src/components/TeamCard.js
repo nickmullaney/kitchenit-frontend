6c4b53649e4f8ae1b42ee2b138a200659275d3ac
nickmullaney <nick.mullaney@gmail.com>
2023-03-06 20:53:41 -0800

    lots of front end fixes

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -15,25 +15,25 @@ class TeamCard extends Component {
     return (
       <div className="teamCardContainer">
 
-        <Card className="teamCard" style={{ width: '18rem' }}>
+        <Card className="teamCard" style={{ width: '20rem' }}>
           <Card.Title as="h2">{authorInfo[0].name}</Card.Title>
           <Card.Img src={Matt} alt='Photos of Team Members' />
           <Card.Text>{authorInfo[0].description}</Card.Text>
         </Card>
 
-        <Card className="teamCard" style={{ width: '18rem' }}>
+        <Card className="teamCard" style={{ width: '20rem' }}>
           <Card.Title as="h2">{authorInfo[1].name}</Card.Title>
           <Card.Img src={Nick} alt='Photos of Team Members' />
           <Card.Text>{authorInfo[1].description}</Card.Text>
         </Card>
 
-        <Card className="teamCard" style={{ width: '18rem' }}>
+        <Card className="teamCard" style={{ width: '20rem' }}>
           <Card.Title as="h2">{authorInfo[3].name}</Card.Title>
           <Card.Img src={Paula} alt='Photos of Team Members' />
           <Card.Text>{authorInfo[3].description}</Card.Text>
         </Card>
 
-        <Card className= "teamCard" style={{ width: '18rem' }}>
+        <Card className= "teamCard" style={{ width: '20rem' }}>
             <Card.Title as="h2">{authorInfo[2].name}</Card.Title>
             <Card.Img src={Jeremy} alt='Photos of Team Members'/>
             <Card.Text>{authorInfo[2].description}</Card.Text>
